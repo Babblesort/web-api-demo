a99ec5ff57f2679a4a822f4b14e7e912748a14f4
Paul McConnell <pmc.consult@gmail.com>
2017-09-09 17:26:12 -0500

    create vs solution

---
+++ web-api-demo/Controllers/FoodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace web_api_demo.Controllers
+{
+    public class FoodsController : ApiController
+    {
+        public object Get()
+        {
+            return null;
+        }
+    }
+}
